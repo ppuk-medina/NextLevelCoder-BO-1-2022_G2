f343c6fb5ec86078ecc4295f7a5ac14851e414b7
ppuk-medina <Jose.Medina@fundacion-jala.org>
2023-03-02 19:22:36 -0400

    Clase 1: Introducción

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,13 @@
+#crear la clase Dinosaur (constructor,update, draw -->pass)
+#
+# Investigar Que son los sprites
+# Investigar que Herencia en programacion
+
+class Dinosaur:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
