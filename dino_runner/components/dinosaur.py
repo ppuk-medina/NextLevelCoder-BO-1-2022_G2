d60035180eb0a0dbed21550e56f9cbda6bc82d7d
ppuk-medina <Jose.Medina@fundacion-jala.org>
2023-03-03 20:02:58 -0400

    CLASE 2

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -5,24 +5,24 @@
 
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, DUCKING
+from dino_runner.utils.constants import RUNNING, DUCKING,JUMPING
 
 class Dinosaur(Sprite):
     POS_X=80
-    POX_Y=310
-    POX_Y_DUCKING=350
-    JUMPING=310
-    JUMP_VELOCITY=8.5
+    POS_Y=310
+    POS_Y_DUCKING=350
+    JUMP_VEL=8.5
     
     def __init__(self):
         self.image=RUNNING[0]
         self.dino_rect=self.image.get_rect()
         self.dino_rect.x=self.POS_X
-        self.dino_rect.y=self.POX_Y
+        self.dino_rect.y=self.POS_Y
         self.step_index=0
         self.dino_running=True
         self.dino_ducking=False
         self.dino_jumping=False
+        self.jump_vel=self.JUMP_VEL
 
     def update(self, user_input):
         if self.dino_running:
@@ -54,24 +54,24 @@ class Dinosaur(Sprite):
         self.image =RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect=self.image.get_rect()
         self.dino_rect.x=self.POS_X
-        self.dino_rect.y=self.POX_Y
+        self.dino_rect.y=self.POS_Y
         self.step_index+=1
         
 
     def jump(self):
-            self.image = JUMPING        
-            if self.dino_jumping:
-                self.dino_rect.y -= self.jump_vel * 4 # Salto            
-                self.jump_vel -= 0.8 # Salto, cuando llega a negativo, baja        
-            if self.jump_vel < -self.JUMP_VEL: # Cuando llega a JUMP_VEL en negativo, este se detiene            
-                self.dino_rect.y = self.Y_POS            
-                self.dino_jumping = False            
-                self.jump_vel = self.JUMP_VEL
+        self.image = JUMPING
+        if self.dino_jumping:
+            self.dino_rect.y -= self.jump_vel * 4 # Salto
+            self.jump_vel -= 0.8 # Salto, cuando llega a negativo, baja
+        if self.jump_vel < -self.JUMP_VEL: # Cuando llega a JUMP_VEL en negativo, este se detiene
+            self.dino_rect.y = self.POS_Y
+            self.dino_jumping = False
+            self.jump_vel = self.JUMP_VEL
 
     def duck(self):
         self.image =DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect=self.image.get_rect()
         self.dino_rect.x=self.POS_X
-        self.dino_rect.y=self.POX_Y_DUCKING
+        self.dino_rect.y=self.POS_Y_DUCKING
         self.step_index+=1
         
